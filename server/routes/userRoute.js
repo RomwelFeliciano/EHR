a94b3fc046388c84881b92331e4e68451bc03e46
elciddelapaz <127919521+RomwelFeliciano@users.noreply.github.com>
2024-02-13 14:18:34 +0800

    Backend: User Route

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,11 +1,19 @@
-// userRoute.js
+// Import express
 const express = require('express');
+
+// Import upload from the multer to use it
 const upload = require('../multerConfig');
+
+// import the functions from the controllers
 const { registerUser, getUsers } = require('../controllers/userController');
 
+// Use the router express
 const router = express.Router();
 
+// Register a user
 router.post('/register', upload.single('profilePicture'), registerUser);
+
+// Get all the users
 router.get('/', getUsers);
 
 module.exports = router;
