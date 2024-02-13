2a91c95a3d23da9bb92196d0df56cfc142534870
elciddelapaz <127919521+RomwelFeliciano@users.noreply.github.com>
2024-02-13 14:19:06 +0800

    Backend: User Controls

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,20 +1,26 @@
+// Import the users schema
 const Users = require('../models/userModel');
+
+// JWT for token when logging in / authentication
 const jwt = require('jsonwebtoken');
+
+// fs for interacting a file in the data
 const fs = require('fs');
 
+// Create a JSON WebToken
 const createToken = (_id) => {
 	return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '3d' });
 };
 
-// Register User
+// Register User in the controller using the statics function in the model
 const registerUser = async (req, res) => {
+	// Destructure the data of request body as an object so we wont call it repeatedly
 	const { firstname, lastname, position, contactNumber, email, password } =
 		req.body;
 
-	// Assuming profilePicture is the name attribute of your file input field
+	// extracting the filename of the file and stores in a constant variable
 	const profilePicture = req.file.filename;
 
-	console.log(profilePicture, req.body);
 	try {
 		// Create a new user document with the profilePicture data
 		const user = await Users.register(
@@ -40,6 +46,7 @@ const registerUser = async (req, res) => {
 	}
 };
 
+// Get all the users for testing in POSTMAN
 const getUsers = async (req, res) => {
 	try {
 		const users = await Users.find({}).sort({ createdAt: -1 });
