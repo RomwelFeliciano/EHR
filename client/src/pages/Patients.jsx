fad043ef258c2cb3a0765920424ec9e4f6237e97
elciddelapaz <127919521+RomwelFeliciano@users.noreply.github.com>
2024-02-15 15:48:01 +0800

    Frontend: Patients Page

---
+++ client/src/pages/Patients.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Patients = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      Patients
+    </div>
+  );
+};
+
+export default Patients;
