6bb4db83a948d666eb661d0cc3059b5ddc77a6c0
elciddelapaz <127919521+RomwelFeliciano@users.noreply.github.com>
2024-02-15 15:48:37 +0800

    Frontend: Logout Function

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,27 @@
-import React, { useState } from "react";
 import { Link } from "react-router-dom";
-const Navbar = ({ isLogin, handeLogout }) => {
+import { useAuthContext } from "../hooks/useAuthContext";
+import { useLogout } from "../hooks/useLogout";
+
+const Navbar = () => {
+  const { user } = useAuthContext();
+  const { logout } = useLogout();
+
+  const handleLogout = () => {
+    logout();
+  };
+
   return (
-    <header className="bg-main fixed top-0 flex h-24 w-full items-center justify-between px-32 shadow-lg">
-      <h1 className="text-second text-3xl font-bold">EHR</h1>
+    <header className="fixed top-0 flex h-24 w-full items-center justify-between bg-main px-32 shadow-lg">
+      <h1 className="text-3xl font-bold text-second">EHR</h1>
       <nav className="flex w-full items-center justify-end gap-4 text-lg font-medium">
         <Link to="/" className="hover:text-second">
           Home
         </Link>
-        {isLogin && (
+        {user && (
           <>
+            <h1>
+              Hello, {user.firstname} {user.lastname}
+            </h1>
             <Link to="/patients" className="hover:text-second">
               Patients
             </Link>
@@ -21,28 +33,28 @@ const Navbar = ({ isLogin, handeLogout }) => {
         <Link to="/about" className="hover:text-second">
           About
         </Link>
-        {!isLogin && (
+        {!user && (
           <>
             <Link
               to="/login"
-              className="text-second border-second hover:bg-third rounded-lg border-2 px-2 py-1 transition-all duration-300 ease-in-out hover:border-black hover:text-white"
+              className="rounded-lg border-2 border-second px-2 py-1 text-second transition-all duration-300 ease-in-out hover:border-black hover:bg-third hover:text-white"
             >
               Login
             </Link>
             <Link
               to="/register"
-              className="text-second border-second hover:bg-third rounded-lg border-2 px-2 py-1 transition-all duration-300 ease-in-out hover:border-black hover:text-white"
+              className="rounded-lg border-2 border-second px-2 py-1 text-second transition-all duration-300 ease-in-out hover:border-black hover:bg-third hover:text-white"
             >
               Register
             </Link>
           </>
         )}
 
-        {isLogin && (
+        {user && (
           <Link
             to="/"
-            className="text-second border-second hover:bg-third rounded-lg border-2 px-2 py-1 transition-all duration-300 ease-in-out hover:border-black hover:text-white"
-            onClick={handeLogout}
+            className="rounded-lg border-2 border-second px-2 py-1 text-second transition-all duration-300 ease-in-out hover:border-black hover:bg-third hover:text-white"
+            onClick={handleLogout}
           >
             Logout
           </Link>
