4997bea21df97e4fb08fae4419cae09357f0b859
elciddelapaz <127919521+RomwelFeliciano@users.noreply.github.com>
2024-02-19 10:28:29 +0800

    Frontend: Patients Table UI

--- client/src/pages/Patients.jsx
+++ client/src/pages/Patients.jsx
@@ -2,8 +2,135 @@ import React from "react";
 
 const Patients = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      Patients
+    <div className="flex min-h-screen items-start justify-center pt-44">
+      <table class="w-full rounded-lg text-left text-sm font-medium text-gray-800 rtl:text-right">
+        <thead class="text-se bg-main text-xs uppercase text-second">
+          <tr>
+            <th scope="col" class="px-6 py-3">
+              Patient's Name
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Birthday
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Hospital Registration #
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Religion
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Address
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Date of Admission
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Chief Complaint
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Admitting Diagnosis
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="border-b transition-all duration-200 ease-in-out odd:bg-white even:bg-pink-100 hover:bg-pink-300">
+            <td
+              scope="row"
+              class="whitespace-nowrap px-6 py-4 font-semibold text-black"
+            >
+              Juan Dela Cruz
+            </td>
+
+            <td class="px-6 py-4">February 29, 2024</td>
+            <td class="px-6 py-4">216</td>
+            <td class="px-6 py-4">Catholic</td>
+            <td class="px-6 py-4">Clark Pampanga</td>
+            <td class="px-6 py-4">March 01, 2024</td>
+            <td class="px-6 py-4">Chief chips</td>
+            <td class="px-6 py-4">Ubo sipon lagnat</td>
+          </tr>
+          <tr class="border-b transition-all duration-200 ease-in-out odd:bg-white even:bg-pink-100 hover:bg-pink-300">
+            <td
+              scope="row"
+              class="whitespace-nowrap px-6 py-4 font-semibold text-black"
+            >
+              Juan Dela Cruz
+            </td>
+
+            <td class="px-6 py-4">February 29, 2024</td>
+            <td class="px-6 py-4">216</td>
+            <td class="px-6 py-4">Catholic</td>
+            <td class="px-6 py-4">Clark Pampanga</td>
+            <td class="px-6 py-4">March 01, 2024</td>
+            <td class="px-6 py-4">Chief chips</td>
+            <td class="px-6 py-4">Ubo sipon lagnat</td>
+          </tr>
+          <tr class="border-b transition-all duration-200 ease-in-out odd:bg-white even:bg-pink-100 hover:bg-pink-300">
+            <td
+              scope="row"
+              class="whitespace-nowrap px-6 py-4 font-semibold text-black"
+            >
+              Juan Dela Cruz
+            </td>
+
+            <td class="px-6 py-4">February 29, 2024</td>
+            <td class="px-6 py-4">216</td>
+            <td class="px-6 py-4">Catholic</td>
+            <td class="px-6 py-4">Clark Pampanga</td>
+            <td class="px-6 py-4">March 01, 2024</td>
+            <td class="px-6 py-4">Chief chips</td>
+            <td class="px-6 py-4">Ubo sipon lagnat</td>
+          </tr>
+          <tr class="border-b transition-all duration-200 ease-in-out odd:bg-white even:bg-pink-100 hover:bg-pink-300">
+            <td
+              scope="row"
+              class="whitespace-nowrap px-6 py-4 font-semibold text-black"
+            >
+              Juan Dela Cruz
+            </td>
+
+            <td class="px-6 py-4">February 29, 2024</td>
+            <td class="px-6 py-4">216</td>
+            <td class="px-6 py-4">Catholic</td>
+            <td class="px-6 py-4">Clark Pampanga</td>
+            <td class="px-6 py-4">March 01, 2024</td>
+            <td class="px-6 py-4">Chief chips</td>
+            <td class="px-6 py-4">Ubo sipon lagnat</td>
+          </tr>
+          <tr class="border-b transition-all duration-200 ease-in-out odd:bg-white even:bg-pink-100 hover:bg-pink-300">
+            <td
+              scope="row"
+              class="whitespace-nowrap px-6 py-4 font-semibold text-black"
+            >
+              Juan Dela Cruz
+            </td>
+
+            <td class="px-6 py-4">February 29, 2024</td>
+            <td class="px-6 py-4">216</td>
+            <td class="px-6 py-4">Catholic</td>
+            <td class="px-6 py-4">Clark Pampanga</td>
+            <td class="px-6 py-4">March 01, 2024</td>
+            <td class="px-6 py-4">Chief chips</td>
+            <td class="px-6 py-4">Ubo sipon lagnat</td>
+          </tr>
+          <tr class="border-b transition-all duration-200 ease-in-out odd:bg-white even:bg-pink-100 hover:bg-pink-300">
+            <td
+              scope="row"
+              class="whitespace-nowrap px-6 py-4 font-semibold text-black"
+            >
+              Juan Dela Cruz
+            </td>
+
+            <td class="px-6 py-4">February 29, 2024</td>
+            <td class="px-6 py-4">216</td>
+            <td class="px-6 py-4">Catholic</td>
+            <td class="px-6 py-4">Clark Pampanga</td>
+            <td class="px-6 py-4">March 01, 2024</td>
+            <td class="px-6 py-4">Chief chips</td>
+            <td class="px-6 py-4">Ubo sipon lagnat</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
