fc7402e033f344295b325755dcce1464a8414d00
elciddelapaz <127919521+RomwelFeliciano@users.noreply.github.com>
2024-02-27 16:12:18 +0800

    Frontend: Add Patient Context Provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
+
 import { BrowserRouter as Router } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
+
 import AppRoutes from "./AppRoutes";
+
 import Navbar from "./components/Navbar";
-import { useState } from "react";
+
 import { loadingContext } from "./contexts/LoadingContext";
-import { SearchProvider } from "./contexts/SearchPatientContext";
+import { PatientContextProvider } from "./contexts/PatientContext";
 import { PatientTableProvider } from "./contexts/PatientTableContext";
+import { SearchProvider } from "./contexts/SearchPatientContext";
 import { ModalProvider } from "./contexts/ModalContext";
 import { ModalFormProvider } from "./contexts/ModalFormContext";
 
@@ -15,18 +20,20 @@ function App() {
   return (
     <Router>
       <loadingContext.Provider value={{ isLoading, setIsLoading }}>
-        <ModalProvider>
-          <ModalFormProvider>
-            <PatientTableProvider>
-              <SearchProvider>
-                <Navbar />
-                <main className="bg-neutral-100 px-32">
-                  <AppRoutes />
-                </main>
-              </SearchProvider>
-            </PatientTableProvider>
-          </ModalFormProvider>
-        </ModalProvider>
+        <PatientContextProvider>
+          <ModalProvider>
+            <ModalFormProvider>
+              <PatientTableProvider>
+                <SearchProvider>
+                  <Navbar />
+                  <main className="bg-neutral-100 px-32">
+                    <AppRoutes />
+                  </main>
+                </SearchProvider>
+              </PatientTableProvider>
+            </ModalFormProvider>
+          </ModalProvider>
+        </PatientContextProvider>
       </loadingContext.Provider>
     </Router>
   );
